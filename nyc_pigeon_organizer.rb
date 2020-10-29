f0833a12d1c97ab9f579f3cdfcac8d276e10cedc
Jaime David <jaimedavid01@yahoo.com>
2020-10-29 23:22:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = 
+  
+  #make a new hash with all the names
+  
+ #identify keys and values of each names
+ 
+  
+  
+  
 end

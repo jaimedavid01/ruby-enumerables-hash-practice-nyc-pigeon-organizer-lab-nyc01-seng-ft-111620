4952f83f2b7bb571e4c0ba73214104d6d33607c3
Jaime David <jaimedavid01@yahoo.com>
2020-10-29 23:45:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   new_hash = {}
   names1 = data.to_a[1][0]
-  names2 = data.to_a[1][1][1]
+  names2 = data.to_a[1][1]
   
   #make a new hash with all the names
   

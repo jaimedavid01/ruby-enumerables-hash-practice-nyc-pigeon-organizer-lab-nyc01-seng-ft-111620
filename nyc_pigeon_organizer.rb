f2aa162e44e8fa3d642e4294a82189986a05da16
Jaime David <jaimedavid01@yahoo.com>
2020-10-30 00:05:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,11 +4,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   #new_hash = {}
  
- names1 = data.to_a[1][1][:male]
- names2 = data.to_a[1][1][:female]
+ names1 = data.to_h[1][1][:male]
+ names2 = data.to_h[1][1][:female]
   names = names1 + names2
   
-  new_hash = names.to_h
+
   #make a new hash with all the names
   
  #identify keys and values of each names

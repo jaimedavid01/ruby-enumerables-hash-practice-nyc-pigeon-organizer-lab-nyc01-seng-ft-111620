848745ae2f42feaf0e4ea53456512a729d659954
Jaime David <jaimedavid01@yahoo.com>
2020-10-30 00:13:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   #new_hash = {}
  
- names1 = data.to_a1][1][:male]
+ names1 = data.to_a[1][1][:male]
  names2 = data.to_a[1][1][:female]
   names1.to_h
   
